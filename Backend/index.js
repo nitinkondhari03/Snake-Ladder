b4b538af9e4e8cc01f04e62969d28739232423e1
Ankesh Chopde <acchopde84@gmail.com>
2023-04-09 21:11:34 +0530

    updateDone

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const connection = require("./configs/db");
 const userRoutes = require("./routes/user.routes");
+const cors = require('cors')
 require("dotenv").config();
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use("/user",userRoutes)
 
